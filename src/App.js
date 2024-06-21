0583fb2be52e4c886ebe5bff395f149463e1ddae
Adityajaiswal03 <git@github.com:Adityajaiswal03/ProductService.git>
2024-06-21 11:01:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React, { useState,useCallback } from 'react';
 function App() {
+  const [input,setInput] = useState('');
+  const [count, setCount] = useState(0);
+  const increament = useCallback(() => {
+      setCount(count + 1);
+  }, [count]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+          ></input>
+          <button onClick={increament}>Increament Counter</button>
+          <h3>Input Text:{input}</h3>
+          <h3>Count:{count}</h3>
+          <hr></hr>
+
+          <ChildComponent count={count} onClick={increament}/>
+      </div>
   );
 }
-
+const ChildComponent = React.memo(({count,onClick}) => {
+  console.log("rerendered Child");
+  return(
+    <div>
+      <h3>Child Component</h3>
+      <h3>Count:{count}</h3>
+      <button onClick={onClick}>Click Me</button>
+    </div>
+  );
+});
 export default App;
